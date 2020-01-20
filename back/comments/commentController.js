86984fc0d22bdf3c611900ab7dcbc43fe34f5cae
bilinskask <55401425+bilinskask@users.noreply.github.com>
2020-01-20 21:26:37 +0200

    Add files via upload

--- back/comments/commentController.js
+++ back/comments/commentController.js
@@ -1,20 +1,39 @@
 const Comment = require('./commentModel.js');
+const Post = require('../posts/postModel')
 
-const addComment = (req, res) => {
-    let data = req.body
-    let comment = new Comment()
-    comment.comment = data.comment;
-    comment.postId = data.postId
-    comment.username = req.user.username
-    comment.save()
-    .then((newComment) => {
-        res.json(newComment)
-    }).catch((e) => {
+const addComment = async (req, res) => {
+    try {
+        let data = await req.body
+        let comment = new Comment()
+        comment.comment = data.comment;
+        comment.postId = data.postId
+        comment.commenterName = req.user.username
+        comment.save()
+
+        let post = await Post.findOne({
+            _id: data.postId
+        })
+        post.comment = comment
+        post.save()
+        res.json(post)
+    } catch (e) {
         res.status(400).json(e)
-    })
+    }
+};
+
+const getCommentsByPostId = async (req, res) => {
+    try {
+        let comments = await Comment.find({
+            postId: req.params.id
+        })
+        res.json(comments)
+    } catch (e) {
+        res.status(400).json(e)
+    }
 };
 
 
 module.exports = {
-    addComment
+    addComment,
+    getCommentsByPostId
 };

b46072ee5ea555c23588ef75ecd03dfd96308b07
Arvydas_Mickus <amickus@icloud.com>
2020-01-22 12:22:46 +0200

    bug fix

--- front/js/profile.js
+++ front/js/profile.js
@@ -24,55 +24,60 @@ const logout = () => {
 };
 
 const postsCounter = () => {
-    let token = localStorage.getItem("x-auth");
-    const noOfPostsSpan = document.querySelector(".statNum");
+  let token = localStorage.getItem("x-auth");
+  const noOfPostsSpan = document.querySelector(".statNum");
+  let activeUserId = localStorage.getItem("activeUserId");
 
-    fetch('http://localhost:3000/api/v1//posts/getAllPosts', {
-      method: "GET",
-      headers: {
-        "x-auth": token,
-        "Content-Type": "application/json"
-      }
-    })
-      .then(response => {
-        // console.log(response);
-    
-        if (!response.ok) {
-          throw Error(response);
-        }
-        return response.json();
-      })
-        .then(allPosts => {
-            const userPosts = allPosts.map(el => {
-                if (el.user[0]._id === activeUserId) {
-                      return el
-                  }
-            })
-            noOfPostsSpan.textContent = userPosts.length
-          })
-}
-
-postsCounter()
-
-const userNameTag = document.getElementById("userNameTag");
-const userProfPicBig = document.querySelector(".userProfPicBig");
-let token = localStorage.getItem("x-auth");
-let activeUserId = localStorage.getItem("activeUserId");
-fetch(`http://localhost:3000/api/v1/user/getSingleUser/${activeUserId}`, {
-    method: 'GET',
+  fetch("http://localhost:3000/api/v1//posts/getAllPosts", {
+    method: "GET",
     headers: {
-        'x-auth': token,
-        'Content-Type': 'application/json'
+      "x-auth": token,
+      "Content-Type": "application/json"
     }
-}).then((response) => {
-    if (!response.ok) {
-      throw Error(response);
-    }
-    return response.json();
   })
-  .then(userFound => {
+    .then(response => {
+      if (!response.ok) {
+        throw Error(response);
+      }
+      return response.json();
+    })
+    .then(allPosts => {
+      const userPosts = allPosts.filter(el => {
+        if (el.user[0]._id === activeUserId) {
+          return el;
+        }
+      });
+      noOfPostsSpan.textContent = userPosts.length;
+    });
+};
+
+postsCounter();
+
+const setProfilePic = () => {
+  const userNameTag = document.getElementById("userNameTag");
+  const userProfPicBig = document.querySelector(".userProfPicBig");
+  let token = localStorage.getItem("x-auth");
+  let activeUserId = localStorage.getItem("activeUserId");
+
+  fetch(`http://localhost:3000/api/v1/user/getSingleUser/${activeUserId}`, {
+    method: "GET",
+    headers: {
+      "x-auth": token,
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw Error(response);
+      }
+      return response.json();
+    })
+    .then(userFound => {
       userNameTag.textContent = userFound.username;
       if (userFound.profilePicURL) {
-          userProfPicBig.src = userFound.profilePicURL;
+        userProfPicBig.src = userFound.profilePicURL;
       }
-  });
+    });
+};
+
+setProfilePic();

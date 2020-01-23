be5d837ce9a46d7d36a43968f1791d6865eb1208
Nikas <nikas@Nikas-MacBook-Pro.local>
2020-01-23 18:32:29 +0200

    meh

--- front/js/feed.js
+++ front/js/feed.js
@@ -38,9 +38,9 @@ feather.replace();
 let moreInfBtn = document.querySelectorAll('.moreBtnPostCom');
 
 for (var i = 0; i < moreInfBtn.length; i++) {
-  moreInfBtn[i].addEventListener('click', (event)=>{
-  let btnClass = event.target.style.display = 'none';
-  });
+    moreInfBtn[i].addEventListener('click', (event) => {
+        let btnClass = event.target.style.display = 'none';
+    });
 }
 
 //checking
@@ -86,20 +86,20 @@ checkifLoggedIn();
 
 //su img ?
 
-const createElements = () =>{
+const createElements = () => {
 
-  let postsCont = document.getElementById('postsCont');
-  let token = localStorage.getItem('x-auth');
-  // let activeUserId = localStorage.getItem('activeUserId')
+    let postsCont = document.getElementById('postsCont');
+    let token = localStorage.getItem('x-auth');
+    // let activeUserId = localStorage.getItem('activeUserId')
 
-  postsCont.innerHTML = '';
+    postsCont.innerHTML = '';
 
-  fetch('http://localhost:3000/api/v1/posts/getAllPosts',{
-    method: 'GET',
-    headers: {
-        'x-auth': token,
-        'Content-Type': 'application/json'
-    }
+    fetch('http://localhost:3000/api/v1/posts/getAllPosts', {
+        method: 'GET',
+        headers: {
+            'x-auth': token,
+            'Content-Type': 'application/json'
+        }
     }).then((response) => {
 
         if (!response.ok) {
@@ -108,181 +108,190 @@ const createElements = () =>{
         return response.json();
     }).then((myJson) => {
 
-      let postsCont = document.getElementById('postsCont');
-      postsCont.innerHTML = '';
+        let postsCont = document.getElementById('postsCont');
+        postsCont.innerHTML = '';
 
-      for (let i = 0; i < myJson.length; i++){
-        //konteineriai
-        let onePost = document.createElement('div')
-        onePost.classList.add('container', 'fullWidthCnt', 'onePost');
+        for (let i = 0; i < myJson.length; i++) {
+            //konteineriai
+            let onePost = document.createElement('div')
+            onePost.classList.add('container', 'fullWidthCnt', 'onePost');
 
-        let userInfoCnt = document.createElement('div')
-        userInfoCnt.classList.add('container-fluid', 'd-flex', 'justify-content-between', 'align-items-center', 'userInfoCnt')
+            let userInfoCnt = document.createElement('div')
+            userInfoCnt.classList.add('container-fluid', 'd-flex', 'justify-content-between', 'align-items-center', 'userInfoCnt')
 
-        //cnt userInfo
-        let userInfo = document.createElement('div')
-        const profileImg = document.createElement('img')
-        profileImg.classList.add('rounded-circle', 'img-fluid', 'userProfPicSml')
-        profileImg.setAttribute('src',"https://www.w3schools.com/w3css/img_avatar3.png")
-        let userName = document.createElement('span')
-        userName.classList.add('font-weight-bold', 'userName')
-        userName.setAttribute("id", "userNameTag");
-        userName.textContent = myJson[i].user[0].username
-        let moreIcn = document.createElement('i')
-        moreIcn.setAttribute("data-feather", "more-horizontal");
+            //cnt userInfo
+            let userInfo = document.createElement('div')
+            const profileImg = document.createElement('img')
+            profileImg.classList.add('rounded-circle', 'img-fluid', 'userProfPicSml')
+            profileImg.setAttribute('src', "https://www.w3schools.com/w3css/img_avatar3.png")
+            let userName = document.createElement('span')
+            userName.classList.add('font-weight-bold', 'userName')
+            userName.setAttribute("id", "userNameTag");
+            userName.textContent = myJson[i].user[0].username
+            let moreIcn = document.createElement('i')
+            moreIcn.setAttribute("data-feather", "more-horizontal");
 
-        //cnt userPost
-        let userPostContentCnt = document.createElement('div')
-        userPostContentCnt.classList.add('container-fluid', 'fullWidthCnt', 'userPostContentCnt')
-        userPostContentCnt.setAttribute("id", "userPost");
+            //cnt userPost
+            let userPostContentCnt = document.createElement('div')
+            userPostContentCnt.classList.add('container-fluid', 'fullWidthCnt', 'userPostContentCnt')
+            userPostContentCnt.setAttribute("id", "userPost");
 
-        const postImg = document.createElement('img')
-        postImg.classList.add('img-fluid', 'postImage')
-        postImg.setAttribute('src', myJson[i].imageURL)
+            const postImg = document.createElement('img')
+            postImg.classList.add('img-fluid', 'postImage')
+            postImg.setAttribute('src', myJson[i].imageURL)
 
-        let postActionsCnt = document.createElement('div')
-        postActionsCnt.classList.add('container-fluid', 'postActionsCnt')
+            let postActionsCnt = document.createElement('div')
+            postActionsCnt.classList.add('container-fluid', 'postActionsCnt')
 
-        let actionsCnt = document.createElement('div')
-        actionsCnt.classList.add('actionsCnt')
+            let actionsCnt = document.createElement('div')
+            actionsCnt.classList.add('actionsCnt')
 
-        let actionsElem = document.createElement('span')
+            let actionsElem = document.createElement('span')
         actionsElem.classList.add('actionsElem')
-        let likeBtn = document.createElement('i')
-        likeBtn.setAttribute("data-feather", "heart");
-        likeBtn.setAttribute("id", "heartBtn");
-        likeBtn.classList.add('actionBtn')
-
-        likeBtn.addEventListener('click', () => {
-          toggleLike(myJson[i]._id)
-          console.log('like')
-      });
-        
-        let actionsElemLink = document.createElement('a')
-        actionsElemLink.classList.add('actionsElemLink')
-        actionsElemLink.addEventListener('click', () => {
-            window.location.href = "../front/comments.html";
+        actionsElem.addEventListener('click', () => {
+            toggleLike(myJson[i]._id, actionsElem)
+            btn = document.getElementById('heartBtn');
+            btn.classList.toggle('fillBtn');
         })
-        let chatIcn = document.createElement('i')
-        chatIcn.setAttribute("data-feather", "message-circle");
-        chatIcn.classList.add('messageCircle')
+        
+            actionsElem.classList.add('actionsElem')
+            let likeBtn = document.createElement('button')
+            likeBtn.setAttribute("data-feather", "heart");
+            likeBtn.setAttribute("id", "heartBtn");
+            likeBtn.classList.add('actionBtn')
 
-        let actionsElemTwo = document.createElement('span')
-        actionsElemTwo.classList.add('actionsElem')
-        let sendBtn = document.createElement('i')
-        sendBtn.setAttribute("data-feather", "send");
-        sendBtn.setAttribute("id", "sendIcon");
-        sendBtn.classList.add('actionBtn')
+            likeBtn.addEventListener('click', () => {
+                toggleLike(myJson[i]._id, actionsElem);
 
-        // cnt postComsAndLikesCnt
-        let postComsAndLikesCnt = document.createElement('div')
-        postComsAndLikesCnt.classList.add('container-fluid', 'postComsAndLikesCnt')
+                likeBtn.classList.toggle('fillBtn');
+          actionsElem.style.background = '#fd1d1d';
+                
+            });
 
-        let userPostCom = document.createElement('div')
-        userPostCom.classList.add('userPostCom')
+            let actionsElemLink = document.createElement('a')
+            actionsElemLink.classList.add('actionsElemLink')
+            actionsElemLink.addEventListener('click', () => {
+                window.location.href = "../front/comments.html";
+            })
+            let chatIcn = document.createElement('i')
+            chatIcn.setAttribute("data-feather", "message-circle");
+            chatIcn.classList.add('messageCircle')
 
-        // let userNameComment = document.createElement('p')
-        // userNameComment.classList.add('font-weight-bold', 'postLikes')
+            let actionsElemTwo = document.createElement('span')
+            actionsElemTwo.classList.add('actionsElem')
+            let sendBtn = document.createElement('i')
+            sendBtn.setAttribute("data-feather", "send");
+            sendBtn.setAttribute("id", "sendIcon");
+            sendBtn.classList.add('actionBtn')
 
-        let userNameComment = document.createElement('span')
-        userNameComment.classList.add('font-weight-bold', 'userName')
-        userNameComment.setAttribute("id", "userNameTag")
+            // cnt postComsAndLikesCnt
+            let postComsAndLikesCnt = document.createElement('div')
+            postComsAndLikesCnt.classList.add('container-fluid', 'postComsAndLikesCnt')
 
-        userNameComment.textContent = myJson[i].user[0].username
-        let comment = document.createElement('span')
-        comment.classList.add('userCommTxt')
-        comment.setAttribute("id", "comment");
-        comment.textContent = myJson[i].title
+            let userPostCom = document.createElement('div')
+            userPostCom.classList.add('userPostCom')
 
-        //main append
-        postsCont.appendChild(onePost)
-        onePost.appendChild(userInfoCnt)
-        //user info append
-        userInfoCnt.appendChild(userInfo)
-        userInfo.appendChild(profileImg)
-        userInfo.appendChild(userName)
-        userInfoCnt.appendChild(moreIcn)
+            // let userNameComment = document.createElement('p')
+            // userNameComment.classList.add('font-weight-bold', 'postLikes')
 
-        //userPost append
-        onePost.appendChild(userPostContentCnt)
-        userPostContentCnt.appendChild(postImg)
-        userPostContentCnt.appendChild(postActionsCnt)
-        postActionsCnt.appendChild(actionsCnt)
-        //userPost icons
-        actionsCnt.appendChild(actionsElem)
-        actionsElem.appendChild(likeBtn)
-        actionsCnt.appendChild(actionsElemLink);
-        actionsElemLink.appendChild(chatIcn)
-        actionsCnt.appendChild(actionsElemTwo);
-        actionsElemTwo.appendChild(sendBtn)
+            let userNameComment = document.createElement('span')
+            userNameComment.classList.add('font-weight-bold', 'userName')
+            userNameComment.setAttribute("id", "userNameTag")
 
-        //postComsAndLikesCnt append
-        onePost.appendChild(postComsAndLikesCnt)
-        postComsAndLikesCnt.appendChild(userPostCom)
-        userPostCom.appendChild(userNameComment)
-        userPostCom.appendChild(comment)
+            userNameComment.textContent = myJson[i].user[0].username
+            let comment = document.createElement('span')
+            comment.classList.add('userCommTxt')
+            comment.setAttribute("id", "comment");
+            comment.textContent = myJson[i].title
 
-        //ikonoms
-        feather.replace();
-      }
+            //main append
+            postsCont.appendChild(onePost)
+            onePost.appendChild(userInfoCnt)
+            //user info append
+            userInfoCnt.appendChild(userInfo)
+            userInfo.appendChild(profileImg)
+            userInfo.appendChild(userName)
+            userInfoCnt.appendChild(moreIcn)
+
+            //userPost append
+            onePost.appendChild(userPostContentCnt)
+            userPostContentCnt.appendChild(postImg)
+            userPostContentCnt.appendChild(postActionsCnt)
+            postActionsCnt.appendChild(actionsCnt)
+            //userPost icons
+            actionsCnt.appendChild(actionsElem)
+            actionsElem.appendChild(likeBtn)
+            actionsCnt.appendChild(actionsElemLink);
+            actionsElemLink.appendChild(chatIcn)
+            actionsCnt.appendChild(actionsElemTwo);
+            actionsElemTwo.appendChild(sendBtn)
+
+            //postComsAndLikesCnt append
+            onePost.appendChild(postComsAndLikesCnt)
+            postComsAndLikesCnt.appendChild(userPostCom)
+            userPostCom.appendChild(userNameComment)
+            userPostCom.appendChild(comment)
+
+            //ikonoms
+            feather.replace();
+        }
     }).catch((e) => {
         console.log(e);
     })
 
 }
-  createElements();
+createElements();
 
 
-const toggleLike = (id, li) => {
-  let token = localStorage.getItem('x-auth');
+const toggleLike = (id, actionsElem) => {
+    let token = localStorage.getItem('x-auth');
 
-  console.log('like')
+    console.log('like')
 
-  fetch(`http://localhost:3000/api/v1/posts/togglelike/${id}`, {
-      method: 'PATCH',
-      headers: {
-          'x-auth': token,
-          'Content-Type': 'application/json'
-      }
-  }).then((response) => {
-
-      li.classList.toggle('list-group-item-success');
-
-      if (!response.ok) {
-          throw Error(response);
-      }
-      return response.json();
-
-  }).then((myJson) => {
-      createElements();
+    fetch(`http://localhost:3000/api/v1/posts/togglelike/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'x-auth': token,
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
 
 
-  }).catch((e) => {
-      console.log(e);
-      alert('toggle failed');
-  });
+        if (!response.ok) {
+            throw Error(response);
+        }
+        return response.json();
+
+    }).then((myJson) => {
+        createElements();
+
+
+    }).catch((e) => {
+        console.log(e);
+        alert('toggle failed');
+    });
 };
 
 const showLikes = (id) => {
-  let token = localStorage.getItem('x-auth');
+    let token = localStorage.getItem('x-auth');
 
-  fetch(`http://localhost:3000/api/v1/posts/getLikesUsers/${id}`, {
-      method: 'GET',
-      headers: {
-          'x-auth': token,
-          'Content-Type': 'application/json'
-      }
-  }).then((response) => {
-      if (!response.ok) {
-          throw Error(response);
-      }
-      return response.json();
+    fetch(`http://localhost:3000/api/v1/posts/getLikesUsers/${id}`, {
+        method: 'GET',
+        headers: {
+            'x-auth': token,
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        if (!response.ok) {
+            throw Error(response);
+        }
+        return response.json();
 
-  }).then((myJson) => {
-      for (let i = 0; i < myJson.length; i++) {
-          console.log(myJson[i].username)
-      };
-  }).catch((e) => {
-      console.log(e);
-  })
+    }).then((myJson) => {
+        for (let i = 0; i < myJson.length; i++) {
+            console.log(myJson[i].username)
+        };
+    }).catch((e) => {
+        console.log(e);
+    })
 };

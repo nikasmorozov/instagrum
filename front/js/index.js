0645b7576c70661404b65fe6279af3132ee2fbdf
Nikas <nikas@Nikas-MBP.lan>
2020-01-27 21:21:16 +0200

    nothing

--- front/js/index.js
+++ front/js/index.js
@@ -175,7 +175,7 @@ const createElements = () => {
                 postLikes.textContent = myJson[i].likes.length-1 + ' likes';
               } else {
                 postLikes.textContent = myJson[i].likes.length + ' likes';
-              }
+              };
               
             } else {
               e.target.classList.replace("ri-heart-line", "ri-heart-fill");
@@ -186,7 +186,7 @@ const createElements = () => {
                 postLikes.textContent = myJson[i].likes.length+1 + ' likes';
               }
               console.log(myJson[i].likes.length)
-            }
+            };
         }
         );
 

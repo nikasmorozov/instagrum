d72b26e13bf11a6a25941417ff4e7d4359d3830c
Nikas <nikas@Nikas-MacBook-Pro.local>
2020-01-30 20:10:31 +0200

    activity kazkas

--- front/js/activity.js
+++ front/js/activity.js
@@ -1,42 +1,6 @@
 // for icons
 feather.replace();
 
-
-// const setProfileInfo = () => {
-//   const userNameTag = document.getElementById("userNameTag");
-//   const userProfPicSml = document.querySelector(".userProfPicSml");
-//   const likedImgActv = document.querySelector(".likedImgActv");
-//   let token = localStorage.getItem("x-auth");
-//   let activeUserId = localStorage.getItem("activeUserId");
-
-//   fetch(`http://localhost:3000/api/v1/user/getSingleUser/${activeUserId}`, {
-//     method: "GET",
-//     headers: {
-//       "x-auth": token,
-//       "Content-Type": "application/json"
-//     }
-//   })
-//     .then(response => {
-//       if (!response.ok) {
-//         throw Error(response);
-//       }
-//       return response.json();
-//     })
-//     .then(userFound => {
-//       userNameTag.textContent = userFound.username + " liked your photo";
-//       if (userFound.profilePicURL) {
-//         userProfPicSml.src = userFound.profilePicURL;
-//       }else{
-//         userProfPicSml.setAttribute(
-//           "src",
-//           "https://www.w3schools.com/w3css/img_avatar3.png"
-//         );
-//       }
-//     });
-// }
-
-// setProfileInfo();
-
 const renderActivities = () => {
 
   let token = localStorage.getItem("x-auth");

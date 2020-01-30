7de325a3f3877d137df239399e100c51e1987861
Arvydas_Mickus <amickus@icloud.com>
2020-01-30 19:54:34 +0200

    delete post image from folder

--- back/posts/postController.js
+++ back/posts/postController.js
@@ -1,140 +1,152 @@
-const Post = require('./postModel.js');
-const User = require('../user/userModel.js');
-const fs = require('fs')
-
+const Post = require("./postModel.js");
+const User = require("../user/userModel.js");
+const fs = require("fs");
 
 const createPost = (req, res) => {
-    const host = req.hostname;
-    const filePath = req.protocol + "://" + host + ":" + req.socket.localPort + '/' + req.file.path;
+  const host = req.hostname;
+  const filePath =
+    req.protocol +
+    "://" +
+    host +
+    ":" +
+    req.socket.localPort +
+    "/" +
+    req.file.path;
 
-    //console.log(filePath);
+  //console.log(filePath);
 
-    let data = req.body
-    let post = new Post()
-    post.title = data.title;
-    post.user = req.user._id;
+  let data = req.body;
+  let post = new Post();
+  post.title = data.title;
+  post.user = req.user._id;
 
-    post.imageURL = filePath
-    post.save()
-    .then((createdPost) => {
-        res.json(createdPost)
-    }).catch((e) => {
-        res.status(400).json(e)
-    })    
+  post.imageURL = filePath;
+  post
+    .save()
+    .then(createdPost => {
+      res.json(createdPost);
+    })
+    .catch(e => {
+      res.status(400).json(e);
+    });
 };
 
 const getAllPosts = async (req, res) => {
-    try {
-        let posts = await Post.find({
-        }).populate({
-            path: 'user',
-            populate: {
-                path: 'followers',
-                match: {
-                    _id: req.user._id
-                }
-            }
-        }).populate({
-            path: 'comment',
-            select: 'comment',
-            populate: { path: 'user', select: 'username' }
-          });
-        res.json(posts)
-    } catch (e) {
-        res.status(400).json(e)
-    }
+  try {
+    let posts = await Post.find({})
+      .populate({
+        path: "user",
+        populate: {
+          path: "followers",
+          match: {
+            _id: req.user._id
+          }
+        }
+      })
+      .populate({
+        path: "comment",
+        select: "comment",
+        populate: { path: "user", select: "username" }
+      });
+    res.json(posts);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 };
 
 const getPostById = async (req, res) => {
-    let postTitle = req.params._id;
-    try {
-        let post = await Post.findOne({
-            post: postTitle
-        })
-        res.json(post)
-
-    } catch (e) {
-        res.status(400).json(e)
-    }
+  let postTitle = req.params._id;
+  try {
+    let post = await Post.findOne({
+      post: postTitle
+    });
+    res.json(post);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 };
 
 const toggleLike = async (req, res) => {
-    let id = req.params.id;
-    let user = req.user.id;
-    try {
-        let post = await Post.findOne({
-            _id: id
-        });
+  let id = req.params.id;
+  let user = req.user.id;
+  try {
+    let post = await Post.findOne({
+      _id: id
+    });
 
-        let isLiked = await Post.findOne({
-            _id: id,
-            likes: user
-        });
-        console.log(isLiked)
+    let isLiked = await Post.findOne({
+      _id: id,
+      likes: user
+    });
+    console.log(isLiked);
 
-        if (!isLiked) {
-        post.likes.push(user)
-        } else {
-            post.likes.pull(user)
-        };
-
-        post.save();
-        res.json(post)
-
-    } catch (e) {
-        res.status(400).json(e)
+    if (!isLiked) {
+      post.likes.push(user);
+    } else {
+      post.likes.pull(user);
     }
+
+    post.save();
+    res.json(post);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 };
 
 const getLikesUsers = async (req, res) => {
-    let id = req.params.id;
+  let id = req.params.id;
 
-    try {
-        let post = await Post.findOne({
-            _id: id
-        });
+  try {
+    let post = await Post.findOne({
+      _id: id
+    });
 
-        let likesUsers = await User.find({
-            _id: post.likes
-        });
+    let likesUsers = await User.find({
+      _id: post.likes
+    });
 
-        res.json(likesUsers)
-    } catch (e) {
-        res.status(400).json(e)
-    }
+    res.json(likesUsers);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 };
 
 const deletePostById = async (req, res) => {
-    let id = req.params.id;
-    try {
-        let post = await Post.findOne({
-            _id: id
-        })
+  let id = req.params.id;
+  try {
+    let post = await Post.findOne({
+      _id: id
+    });
 
-        let oldPicPath = post.imageURL
-    .split(`${req.protocol + "://" + req.hostname + ":" + req.socket.localPort + "/"}`)
-    .pop();
-  fs.unlink(oldPicPath, err => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
-  res.json(post)
-        
-  post.delete()
-        
-        
-    } catch (e) {
-        res.status(400).json(e)
-    }
+    let oldPicPath = post.imageURL
+      .split(
+        `${req.protocol +
+          "://" +
+          req.hostname +
+          ":" +
+          req.socket.localPort +
+          "/"}`
+      )
+      .pop();
+    fs.unlink(oldPicPath, err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+    res.json(post);
+
+    post.delete();
+  } catch (e) {
+    res.status(400).json(e);
+  }
 };
 
 module.exports = {
-    createPost,
-    getAllPosts,
-    getPostById,
-    deletePostById,
-    toggleLike,
-    getLikesUsers
+  createPost,
+  getAllPosts,
+  getPostById,
+  deletePostById,
+  toggleLike,
+  getLikesUsers
 };
